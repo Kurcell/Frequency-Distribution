39c4b9f153d1cbe05196afdff5b0c3110d178d05
owner <owner@ThinkBoi>
2020-04-22 17:15:13 -0400

    Base Project

---
+++ Project2/src/ciic4020/project2/strategiesClasses/MapFD.java
@@ -0,0 +1,19 @@
+package ciic4020.project2.strategiesClasses;
+
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+public class MapFD<E extends Comparable<E>> extends AbstractFDStrategy<E> {
+
+	public MapFD() {
+		super("Map");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<Entry<E, Integer>> computeFDList(ArrayList<E> dataSet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
